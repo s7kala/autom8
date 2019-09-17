1ad01f8825062c56644e6ef6d03c22ded81830de
s7kala <qft360@gmail.com>
2019-09-17 17:53:28 -0400

    second commit

---
+++ portscn.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd, port;
+    struct hostent *he;
+
+  /*  if (argc != 2)
+    {
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
+    }
+*/
+    if ((he=gethostbyname(argv[1])) == NULL)   
+    {
+        perror("gethostbyname");
+        exit(1);
+    }
+    for(port=0; port<=65000; port++)
+    {
+        struct sockaddr_in their_addr; 
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            perror("socket");
+            exit(1);
+        }
+
+        their_addr.sin_family = AF_INET;    
+        their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+        memset(&(their_addr.sin_zero), '\0', 8); 
+
+        if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1)
+        {
+            close(sockfd);
+        }
+        else
+        {
+            printf("%i open\n", port);
+            close(sockfd);
+        }
+    }
+}
